035bf7776ac19ba927e57dcd87b297c96dd54eeb
Alen Murtić <murtic.alen@hotmail.com>
2018-04-18 18:11:19 +0200

    [R:Dataset] Added extracting weekday from date (#3)

--- Java/src/dataset/util/CallRecord.java
+++ Java/src/dataset/util/CallRecord.java
@@ -8,23 +8,23 @@ public class CallRecord {
     private String location;
     private String duration;
     private String callTime;
-    private String originCarrierId;
-    private String terminatingCarrierId;
+    //private String originCarrierId;
+    //private String terminatingCarrierId;
     public static final String DELIMITER = ";";
-    public static final String HEADER = "id" + DELIMITER + "callerId" + DELIMITER + "receiverId" + DELIMITER + "location" + DELIMITER + "duration" + DELIMITER + "callTime" + DELIMITER + "originCarrierId" + DELIMITER + "terminatingCarrierId";
+    public static final String HEADER = "id" + DELIMITER + "callerId" + DELIMITER + "receiverId" + DELIMITER + "location" + DELIMITER + "duration" + DELIMITER + "callTime";// + DELIMITER + "originCarrierId" + DELIMITER + "terminatingCarrierId";
 
-    public CallRecord(String id, String callerId, String receiverId, String location, String duration, String callTime, String originCarrierId, String terminatingCarrierId) {
+    public CallRecord(String id, String callerId, String receiverId, String location, String duration, String callTime){//, String originCarrierId, String terminatingCarrierId) {
         this.id = id;
         this.callerId = callerId;
         this.receiverId = receiverId;
         this.location = location;
         this.duration = duration;
         this.callTime = callTime;
-        this.originCarrierId = originCarrierId;
-        this.terminatingCarrierId = terminatingCarrierId;
+        //this.originCarrierId = originCarrierId;
+        //this.terminatingCarrierId = terminatingCarrierId;
     }
 
     public String toString() {
-        return id + DELIMITER + callerId + DELIMITER + receiverId + DELIMITER + location + DELIMITER + duration + DELIMITER + callTime + DELIMITER + originCarrierId + DELIMITER + terminatingCarrierId;
+        return id + DELIMITER + callerId + DELIMITER + receiverId + DELIMITER + location + DELIMITER + duration + DELIMITER + callTime;// + DELIMITER + originCarrierId + DELIMITER + terminatingCarrierId;
     }
 }
